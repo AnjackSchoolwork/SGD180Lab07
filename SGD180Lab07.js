1529a0c8def32bac6e7abf62805ab6f89745dfd5
Anjack <gary.reif@gmail.com>
2017-09-28 12:03:12 -0400

    Initial commit.

---
+++ SGD180Lab07.js
@@ -0,0 +1,25 @@
+
+
+
+/**
+ * Initialize the environment. Create the canvas and load the config.
+ */
+function setup() {
+	game = new Scene()
+
+
+	game.start()
+}
+
+
+function update() {
+
+}
+
+function generateMob() {
+
+}
+
+function generateInteractable() {
+
+}
